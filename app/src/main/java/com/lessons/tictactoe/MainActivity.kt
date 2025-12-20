44427c695e932b2569cd24625d17f9068b85ccac
Ido Naor <idonaor15.do@gmail.com>
2025-12-20 18:07:59 +0200

    created buttons grid and the general layout with play again button and text view

--- app/src/main/java/com/lessons/tictactoe/MainActivity.kt
+++ app/src/main/java/com/lessons/tictactoe/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.lessons.tictactoe
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttons: Array<Button>
+    private var currentPlayer = "X"
+    private var board = Array(9) { "" }
+    private var gameActive = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
